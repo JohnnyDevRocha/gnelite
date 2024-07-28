3077eddcd49dd64dbec27ea54367a32be4d7bbca
JohnnyDev2001 <johnnydevcode@gmail.com>
2024-07-28 12:35:36 -0300

    Bootstrap e outras dependencias

--- routers.php
+++ routers.php
@@ -4,6 +4,6 @@ use Pecee\SimpleRouter\SimpleRouter as router;
 
 router::setDefaultNamespace('src\Controllers');
 
-router::get('/', "homeController@index");
+router::get(BASE_URL, "homeController@index");
 
 router::start();
